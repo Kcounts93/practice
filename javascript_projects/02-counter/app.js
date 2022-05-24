076132d0347213971476630733ce1197ea2824a9
Kayla <95451731+Kcounts93@users.noreply.github.com>
2022-05-23 17:08:44 -0700

    Counter Project - DONE

--- javascript_projects/02-counter/app.js
+++
@@ -1,8 +0,0 @@
-// set intital count
-let count = 0;
-
-// select value and buttons
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll("btn");
-
-console.log(btns);
