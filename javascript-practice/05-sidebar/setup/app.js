4551f308fa900fd47983fb94f03f09201db99b13
Kayla <95451731+Kcounts93@users.noreply.github.com>
2022-05-31 20:56:55 -0700

    modal-done

--- javascript-practice/05-sidebar/setup/app.js
+++ javascript-practice/05-sidebar/setup/app.js
@@ -13,5 +13,5 @@ toggleBtn.addEventListener("click", function () {
 });
 
 closeBtn.addEventListener('click', function () {
-    sidebar.classList.remove('show-sidebar');
+    sidebar.classList.remove('show-sidebar');   
 });
